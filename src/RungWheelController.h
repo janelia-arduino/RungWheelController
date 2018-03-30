342c68eef22a7cda96d4209c8b93db16f9390983
Peter Polidoro <peterpolidoro@gmail.com>
2018-03-30 14:12:11 -0400

    Removed unnecessary header includes.

--- src/RungWheelController.h
+++ src/RungWheelController.h
@@ -7,23 +7,12 @@
 // ----------------------------------------------------------------------------
 #ifndef RUNG_WHEEL_CONTROLLER_H
 #define RUNG_WHEEL_CONTROLLER_H
-#include <Flash.h>
-#include <EEPROM.h>
-#include <Streaming.h>
 #include <ArduinoJson.h>
-#include <JsonSanitizer.h>
 #include <JsonStream.h>
 #include <Array.h>
 #include <Vector.h>
-#include <MemoryFree.h>
 #include <ConstantVariable.h>
-#include <SavedVariable.h>
 #include <Functor.h>
-#include <IndexedContainer.h>
-#include <FunctorCallbacks.h>
-#include <Watchdog.h>
-
-#include <EventController.h>
 
 #include <ModularClient.h>
 #include <OpticalSwitchInterface.h>
