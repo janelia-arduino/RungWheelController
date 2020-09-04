3b8fa53c20ccdc1b996c02e3dbc59a274919189a
Peter Polidoro <peterpolidoro@gmail.com>
2020-09-04 16:40:35 -0400

    Update to work with ArduinoJson 6

--- src/RungWheelController/RungWheelController.cpp
+++ src/RungWheelController/RungWheelController.cpp
@@ -132,8 +132,8 @@ bool RungWheelController::flipEnabled()
 // floating-point number (float, double)
 // bool
 // const char *
-// ArduinoJson::JsonArray *
-// ArduinoJson::JsonObject *
+// ArduinoJson::JsonArray
+// ArduinoJson::JsonObject
 // const ConstantString *
 //
 // For more info read about ArduinoJson parsing https://github.com/janelia-arduino/ArduinoJson
